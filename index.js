34d63efd4c8833c6825b6c838c9879e6e19b3b9b
ChrisWawerusc <wawerubusiness3@gmail.com>
2024-04-22 13:39:17 +0300

    can now send files and view metadata

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
 var app = express();
@@ -10,9 +11,25 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'public/') // Files will be saved in the 'uploads' directory
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
 
+// Initialize Multer with storage options
+const upload = multer({ storage: storage });
 
+app.post("/api/fileanalyse", upload.single('upfile'),(req,res)=>{
+  const filename = req.file.originalname; // Using file.originalname directly
+  const filesize = req.file.size; // Using file.size directly
+  const filetype = req.file.mimetype;
 
+  res.json({name:filename,type:filetype, size:filesize})
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
